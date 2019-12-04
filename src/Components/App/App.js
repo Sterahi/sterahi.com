6795c04c814fdbe2013f347fb4c53af9f2eebfd5
Elizabeth Murray <liz@sterahi.com>
2019-12-04 14:04:20 -0500

    update homepage, footer & add scroll to top

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link }  from 'react-router-dom'
-import HttpsRedirect from 'react-https-redirect'
+import React from "react"
+import { BrowserRouter as Router, Route, Switch, Link }  from "react-router-dom"
+import HttpsRedirect from "react-https-redirect"
 
-import Articles from '../Articles/Articles'
-import Article from '../Article/Article'
-import Home from '../Home/Home'
+import Articles from "../Articles/Articles"
+import Article from "../Article/Article"
+import Home from "../Home/Home"
+import ScrollToTop from "../ScrollToTop/ScrollToTop"
 
-import logo_transparent from './logo_transparent.png'
-import './App.scss';
+import logo_transparent from "./logo_transparent.png"
+import "./App.scss"
 
 export default function AppRouter() {
     return (
         <>
             <HttpsRedirect>
                 <Router>
-                    <nav>
-                        <div className = "navigation">
-                            <ul>
-                                <li>
-                                    <Link to ="/">Home</Link>
-                                </li>
-                                <li>
-                                    <Link to = "/projects">Projects</Link>
-                                </li>
-                            </ul>
+                    <section>
+                        <nav>
+                            <div className = "navigation">
+                                <ul>
+                                    <li>
+                                        <Link to ="/">Home</Link>
+                                    </li>
+                                    <li>
+                                        <Link to = "/projects">Projects</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+
+                        <div className = "hero">
+                            <h1><img src = { logo_transparent } className = "logo" alt = "logo" /></h1>
                         </div>
-                    </nav>
 
-                    <div className = "hero">
-                        <h1><img src = { logo_transparent } className = 'logo' alt = 'logo' /></h1>
-                    </div>
-
-                    <Switch>
-                        <Route path = "/project/:slug" component = {Article} />
-                        <Route path = '/projects' component = { Articles } />
-                        <Route path = '/' component = { Home }/>
-                    </Switch>
+                        <Switch>
+                            <Route path = "/project/:slug" component = {Article} />
+                            <Route path = "/projects" component = { Articles } />
+                            <Route path = "/" component = { Home }/>
+                        </Switch>
+                    </section>
+                    <ScrollToTop />
                 </Router>
                 <footer>
                     <div>Built by Elizabeth (Sterahi) Murray</div>
--- src/Components/VizAwareCard/VizAwareCard.js
+++ src/Components/VizAwareCard/VizAwareCard.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react"
+import { Link } from "react-router-dom"
 
-import VizSensor from 'react-visibility-sensor'
+import VizSensor from "react-visibility-sensor"
 
-import { faCode } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-import './VizAwareCard.scss'
+import "./VizAwareCard.scss"
 
 export default class VizAwareCard extends React.Component {
-    state = {
-        cardViz: false,
-        once: false
+    constructor() {
+        super()
+        this.state = {
+            cardViz: false,
+            once: false
+        }
     }
 
     onChangeVisibility(isVisible) {
@@ -36,23 +39,23 @@ export default class VizAwareCard extends React.Component {
                         this.setState({
                             cardViz: isVisible
                         })
-                }
-            }>
+                    }
+                }>
                 <div className = 'card'
                     style = {{
                         background: `url(${project.image.fields.file.url}) center center / cover no-repeat`,
                         opacity: this.state.cardViz ? 1: .25,
-                        position: 'relative',
+                        position: "relative",
                         left: this.state.cardViz ? 0: 300,
-                        transition: 'ease all 1s'
-                }}>
+                        transition: "ease all 1s"
+                    }}>
                     <div className = "cardInner">
                         <span>{project.projectName}</span>
                         <Link to = {{
-                                pathname: `/project/${project.slug}`,
-                                project: project
-                            }}
-                            className = "button">
+                            pathname: `/project/${project.slug}`,
+                            project: project
+                        }}
+                        className = "button">
                             See Details!
                         </Link>
                         {codeAvaliable}
